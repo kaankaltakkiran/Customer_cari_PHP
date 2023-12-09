6dc800104b8da88e384915523c98613a49ecf98d
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-12-09 21:53:41 +0300

    added basic footer

--- index.php
+++ index.php
@@ -11,8 +11,9 @@ $activePage="index";
     <title>Home</title>
     <link rel="icon" type="image/x-icon" href="./public/img/favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
   </head>
-  <body>
+  <body class="bg-light.bg-gradient">
    <!--Ortak navbar -->
   <?php
     require_once('navbar.php');?>
@@ -66,6 +67,9 @@ $companys = $SORGU->fetchAll(PDO::FETCH_ASSOC);
 </div>
 <?php } ?>
     </div>
+    <!--Ortak footer -->
+    <?php
+    require_once('footer.php');?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
