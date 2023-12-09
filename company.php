c9c613600286258c544ac362c5a66bed7ae29804
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-12-09 15:37:19 +0300

    remove button not used

--- company.php
+++ company.php
@@ -95,12 +95,10 @@ echo "
 </div>
 
 <a href="index.php" class="btn btn-warning">Back To Home</a>
-<div class="text-center">
-  <a type="button" href="cariAction.php"  class="btn btn-primary mt-2">Cari Action</a>
 </div>
-<div class="text-center">
+<!-- <div class="text-center">
   <a type="button" href="transerMoney.php"  class="btn btn-danger mt-2">Transfer Money</a>
-</div>   
+</div>  -->  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
