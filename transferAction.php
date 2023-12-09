4bcc587e3d2357dd403f4e8cdb2b90338954823d
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-12-09 15:59:35 +0300

    fixed pagination with datatables

--- transferAction.php
+++ transferAction.php
@@ -10,6 +10,8 @@ require 'loginControl.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Transfer Actions</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css" rel="stylesheet">
   </head>
   <body>
 <?php
@@ -23,8 +25,8 @@ require 'navbar.php';
 </div>
 </div>
 <!-- table-responsive -->
-<div class="table-responsive">
-<table class="table table-bordered table-striped ">
+<div class="">
+<table id="example" class="table table-bordered table-striped ">
 <thead>
 <tr>
 <th>CompanyId</th>
@@ -76,6 +78,12 @@ echo "
 
 <a href="index.php" class="btn btn-warning">Back To Home</a> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+      new DataTable('#example');
+    </script>
   </body>
 </html>
 
