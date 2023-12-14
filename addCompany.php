0eaece80e3d5522d6a851c09c9236f5caea94e96
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-12-14 11:13:40 +0300

    fixed folder design and add js folder

--- addCompany.php
+++ addCompany.php
@@ -90,6 +90,6 @@ if(isset($_POST['form_companyName'])){
 </div>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="autoCloseAlert.js"></script>
+    <script src="./public/js/autoCloseAlert.js"></script>
   </body>
 </html>
--- register.php
+++ register.php
@@ -57,7 +57,7 @@ if (isset($_POST['form_email'])) {
 </div>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="hideShow.js"></script>
+    <script src="./public/js/hideShow.js"></script>
   </body>
 </html>
 <!-- Kullanıcı üye bilgilerini veri tabanına kaydetme -->
