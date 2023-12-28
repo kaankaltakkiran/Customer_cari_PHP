ed9b28917400eafb238752d7242736086b45bdf1
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-12-28 15:03:43 +0300

    added login page user information

--- login.php
+++ login.php
@@ -152,6 +152,37 @@ INCORRECT EMAIL Or PASSWORD!...
      </form>
      </div>
 </div>
+<div class="row justify-content-end ">
+  <div class="col-6 ">
+  <h3 class="text-center text-info">User İnformation</h3>
+<table class="table  table-striped">
+  <thead>
+    <tr>
+    <th scope="col">#</th>
+      <th scope="col">Email</th>
+      <th scope="col">Password</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>veli@gmail.com</td>
+      <td>123</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>ahmet@gmail.com</td>
+      <td>123</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>selin@gmail.com</td>
+      <td>123</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</div>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="./Public/js/hideShow.js"></script>
